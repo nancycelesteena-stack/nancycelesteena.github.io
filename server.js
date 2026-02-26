1dbf0069fbdb7b927ff376f08de94b6c8139452c
nancycelesteena-stack <nancycelesteena@gmail.com>
2026-02-26 10:14:54 +0530

    update project code

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Database connection
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "nancy",
+  database: "portfolio_db"
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Database connection failed:", err);
+  } else {
+
+    console.log("Connected to MySQL");
+  }
+});
+
+// API to store contact form
+app.post("/contact", (req, res) => {
+  const { name, email, message } = req.body;
+
+  const sql = "INSERT INTO contacts (name, email, message) VALUES (?, ?, ?)";
+
+  db.query(sql, [name, email, message], (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error saving data");
+    } else {
+      res.send("Message saved successfully");
+    }
+  });
+});
+
+const PORT = process.env.PORT || 10000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "../frontend")));
+
+
+password:""
+
+
+
